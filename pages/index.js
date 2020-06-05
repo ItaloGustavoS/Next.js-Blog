520b00040ce07ccf34d04d0a093ddb25b744ead1
Ítalo <42496107+ItaloGustavoS@users.noreply.github.com>
2020-06-04 22:55:14 -0300

    Correção dos Links

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
-import Link from 'next/link'
-import Date from '../components/date'
-
+import Link from "next/link";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -29,26 +27,18 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
       </section>
     </Layout>
-    <li className={utilStyles.listItem} key={id}>
-    <Link href="/posts/[id]" as={`/posts/${id}`}>
-      <a>{title}</a>
-    </Link>
-    <br />
-    <small className={utilStyles.lightText}>
-      <Date dateString={date} />
-    </small>
-  </li>
-  
   );
 }
 
